e5223ccc173684f5944e1de360efb4c1dfd15338
MarieWS <mwosaz@gmail.com>
2025-01-15 12:06:17 +0100

    Create meal-plan.js

---
+++ js/meal-plan.js
@@ -0,0 +1,26 @@
+function readExcelFile(file) {
+    const reader = new FileReader();
+    reader.onload = function (event) {
+        const data = new Uint8Array(event.target.result);
+        const workbook = XLSX.read(data, { type: 'array' });
+        const sheetName = workbook.SheetNames[0];
+        const worksheet = workbook.Sheets[sheetName];
+        const json = XLSX.utils.sheet_to_json(worksheet);
+        displayData(json);
+    };
+    reader.readAsArrayBuffer(file);
+}
+
+function displayData(data) {
+    const container = document.getElementById('excel-data');
+    data.forEach(row => {
+        const div = document.createElement('div');
+        div.textContent = JSON.stringify(row);
+        container.appendChild(div);
+    });
+}
+
+document.getElementById('upload').addEventListener('change', function (event) {
+    const file = event.target.files[0];
+    readExcelFile(file);
+});
