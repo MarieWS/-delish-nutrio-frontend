28d365bf6d7ea27fd641959eef0a67fef54073b6
MarieWS <mwosaz@gmail.com>
2025-01-15 16:06:44 +0100

    Update register.js

--- js/register.js
+++ js/register.js
@@ -5,6 +5,7 @@ const formOpenBtn = document.querySelector("#form-open"),
   signupBtn = document.querySelector("#signup"),
   loginBtn = document.querySelector("#login"),
   pwShowHide = document.querySelectorAll(".pw_hide");
+
 formOpenBtn.addEventListener("click", () => home.classList.add("show"));
 formCloseBtn.addEventListener("click", () => home.classList.remove("show"));
 pwShowHide.forEach((icon) => {
@@ -26,4 +27,40 @@ signupBtn.addEventListener("click", (e) => {
 loginBtn.addEventListener("click", (e) => {
   e.preventDefault();
   formContainer.classList.remove("active");
+});
+
+const form = document.getElementById('register-form');
+
+form.addEventListener('submit', function (event) {
+    event.preventDefault();
+
+    const formData = new FormData(form);
+    const data = Object.fromEntries(formData);
+
+    const options = {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+            'accept': 'application/json'
+        },
+        body: JSON.stringify(data),
+    }
+
+    fetch('https://delish-nutrio.onrender.com/api/register', options)
+        .then(response => {
+            if (!response.ok) {
+                return response.text().then(text => {
+                    console.log(text);
+                    throw new Error(`HTTP error! Status: ${response.status}`);
+                });
+            }
+            return response.json();
+        })
+        .then(data => {
+            console.log('Success:', data);
+            window.location.href = 'verifyEmail.html';  // Redirect to verification page
+        })
+        .catch(error => {
+            console.error('Error:', error);
+        });
 });
