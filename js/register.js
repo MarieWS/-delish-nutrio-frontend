77fbc03d48441d71f5ad303bc474d93154ae01f4
MarieWS <mwosaz@gmail.com>
2025-01-17 18:31:56 +0100

    Update register.js

--- js/register.js
+++ js/register.js
@@ -1,40 +1,42 @@
 import { backendServer } from "./global_variables.js";
+
 const form = document.getElementById('register-form');
 
-form.addEventListener('submit', function (event) {
-    event.preventDefault();
-    const formData = new FormData(form);
-    const formDataObject = Object.fromEntries(formData);
+form.addEventListener('submit', async (event) => {
+  event.preventDefault();
 
-    const options = {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json'
-        },
-        body: JSON.stringify(formDataObject),
+  const formData = new FormData(form);
+  const formDataObject = Object.fromEntries(formData);
+
+  try {
+    const response = await fetch(`${backendServer}/api/register`, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(formDataObject),
+    });
+
+    if (response.ok) {
+      window.alert('User Registered Successfully');
+      window.location.href = './verification.html'; 
+    } else {
+      const data = await response.json(); 
+      if (data.message) {
+        alert(data.message); 
+      } else if (data.errors) {
+        data.errors.forEach(error => {
+          const errorElement = document.getElementById(`${error.path}_error`);
+          if (errorElement) {
+            errorElement.textContent = error.msg;
+          }
+        });
+      } else {
+        alert('An error occurred during registration.');
+      }
     }
 
-    const register = async () => {
-        try {
-            const response = await fetch(`${backendServer}/api/register`, options);
-
-            if (response.ok) {
-                window.alert('User Registered Successfully');
-                window.location.href = './verification.html';
-            } else {
-                const responseData = await response.json();
-                const errors = responseData.errors
-
-                errors.forEach(error => {
-                    document.querySelector(`#${error.path}_error`).textContent = error.msg
-                });
-            }
-
-        } catch (error) {
-            window.alert('An error occurred')
-        }
-
-    }
-    register()
-
-})
+  } catch (error) {
+    alert('An error occurred during registration.');
+  }
+});
