37c3091ba4d637b453f4dedc59933c6c39b03b55
MarieWS <mwosaz@gmail.com>
2025-01-17 13:57:35 +0100

    Update register.js

--- js/register.js
+++ js/register.js
@@ -1,41 +1,40 @@
-document.addEventListener('DOMContentLoaded', () => {
-    const registerForm = document.getElementById('register-form');
-    
-    registerForm.addEventListener('submit', (e) => {
-        e.preventDefault();
-        
-        const firstName = registerForm.elements['firstname'].value;
-        const lastName = registerForm.elements['lastname'].value;
-        const username = registerForm.elements['username'].value;
-        const phoneNumber = registerForm.elements['phone_number'].value;
-        const email = registerForm.elements['email'].value;
-        const password = registerForm.elements['password'].value;
-        const confirmPassword = registerForm.elements['confirmPassword'].value;
-        
-        if (password !== confirmPassword) {
-            alert('Passwords do not match.');
-            return;
-        }
-        
-        fetch('https://techcrushbackend-85eulpiau-maries-projects-0395c141.vercel.app/api/register/api/register', {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json',
-                'Accept': 'application/json'
-            },
-            body: JSON.stringify({ firstName, lastName, username, phoneNumber, email, password })
-        })
-        .then(response => response.json())
-        .then(data => {
-            if (data.success) {
-                window.location.href = 'accountcreationsuccess.html';
+import { backendServer } from "./global_variables.js";
+const form = document.getElementById('register-form');
+
+form.addEventListener('submit', function (event) {
+    event.preventDefault();
+    const formData = new FormData(form);
+    const formDataObject = Object.fromEntries(formData);
+
+    const options = {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(formDataObject),
+    }
+
+    const register = async () => {
+        try {
+            const response = await fetch(`${backendServer}/api/register`, options);
+
+            if (response.ok) {
+                window.alert('User Registered Successfully');
+                window.location.href = './verification.html';
             } else {
-                alert('Registration failed. Please try again.');
+                const responseData = await response.json();
+                const errors = responseData.errors
+
+                errors.forEach(error => {
+                    document.querySelector(`#${error.path}_error`).textContent = error.msg
+                });
             }
-        })
-        .catch(error => {
-            console.error(error);
-            alert('An error occurred. Please try again.');
-        });
-    });
-});
+
+        } catch (error) {
+            window.alert('An error occurred')
+        }
+
+    }
+    register()
+
+})
